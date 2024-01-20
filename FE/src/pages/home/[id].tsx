e8421e1a626081379fa84b0da4a56d8d1d882634
GiuseppePitruzzella <giuseppepitru@icloud.com>
2024-01-21 00:24:03 +0100

    :sparkles: Add Fetch Projects

--- FE/src/pages/home/[id].tsx
+++ FE/src/pages/home/[id].tsx
@@ -3,16 +3,23 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import CardComponent from '../components/CardComponent';
 
-interface Card {
+interface User {
     id: number;
     name: string;
     email: string;
     job: string;
 }
 
-const Home: React.FC<{ card: Card }> = ({ card }) => {
+interface Project {
+    id: number;
+    id_user: number;
+    name: string;
+    description: string;
+}
+
+const Home = () => {
     const router = useRouter();
-    const { id, backendName, apiUrl, userData } = router.query;
+    const { userData, backendName, apiUrl } = router.query;
     const user = JSON.parse(userData as string);
 
     const backgroundColors: { [key: string]: string } = {
@@ -23,44 +30,41 @@ const Home: React.FC<{ card: Card }> = ({ card }) => {
         flask: 'bg-[#E0EAF5] hover:bg-[#DEE6F0]',
     };
 
-    // const [user, setUser] = useState<User | null>(null);
+    const [projects, setProjects] = useState<Project[]>([]);
+    // const [newUser, setNewUser] = useState({ name: '', email: '', job: '' });
+    // const [updateUser, setUpdateUser] = useState({ id: '', name: '', email: '', job: '' });
+    
+    // Fetch Projects
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${apiUrl}/api/${backendName}/projects`);
+                console.log(response);
+                setProjects(response.data.reverse());
+            } catch (error) {
+                console.error('Error Fetching Data: ', error);
+            }
+        };
 
-    // // Fetch Users
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-    //             const response = await axios.get(`${apiUrl}/api/${backendName}/users/${id}`);
-    //             console.log("OK: ", response.data)
-    //             if (response.data) {
-    //                 setUser({ 
-    //                     id: response.data.id, 
-    //                     name: response.data.name, 
-    //                     email: response.data.email, 
-    //                     job: response.data.job 
-    //                 });
-    //             }
-    //         } catch (error) {
-    //             console.error('Error Fetching Data: ', error);
-    //         }
-    //     };
-
-    //     fetchData();
-    // }, [id, backendName, apiUrl]);
+        fetchData();
+    }, [backendName, apiUrl]);
+    
 
     return (
         <div>
-            {/* Contenuto della tua pagina home */}
-            <p>ID: {user.id}</p>
-
-            {/* Display Users */}
-            
-            {/* <div key={user.id} className={`flex items-center justify-between ${buttonColors} shadow-neo w-64 rounded-2xl hover:shadow-inner-neo`}>
-                {user.id}
-            </div> */}
             <div className="p-2">
                 <h1 className="text-base font-sm leading-7 tracking-tight text-gray-800">{user.name}</h1>
                 <p className="text-sm font-sm leading-6 text-gray-500">{user.job}</p>
             </div>
+            <div>
+                {projects.map((project) => (
+                    <div key={project.id} className={`flex items-center justify-between ${buttonColors} shadow-neo w-64 rounded-2xl hover:shadow-inner-neo`}>
+                        <h1 className="text-base font-sm leading-7 tracking-tight text-gray-800">{project.name}</h1>
+                        <p className="text-sm font-sm leading-6 text-gray-500">{project.description}</p>
+                    </div>
+                ))}
+            </div>
+
         </div>
     );
 };
